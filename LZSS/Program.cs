b3512b3584d04db3ffb309b0e595dfc264afafe2
misiaxis <michal.pr@o2.pl>
2018-11-12 09:44:18 +0100

    Przykład konsolowy

---
+++ LZSS/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LZSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"f:\input.txt");
+
+            string compressed=LZSS.CompressLZSS(input);
+
+
+            var decompressed = LZSS.DecompressLZSS(compressed);
+
+            int compresion = (compressed.Length)*100/input.Length;
+            bool sth = (input.Equals(decompressed));
+
+            Console.WriteLine(input);
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine(compressed);
+
+            Console.WriteLine(Environment.NewLine+Environment.NewLine+Environment.NewLine);
+
+            if (sth)
+            {
+                Console.WriteLine("Teksty się zgadzają");
+            }
+            else
+            {
+                Console.WriteLine("Teksty się nie zgadzają");
+            }
+
+            Console.WriteLine("Kompresja: "+compresion+"%");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
